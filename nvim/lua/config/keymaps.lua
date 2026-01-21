b104b5b00c774cc7ec4307051ca122b60999172c
Pearse Hutson <pearse.2008@gmail.com>
2026-01-21 23:55:28 +0000

    Improve clipboard with host

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,21 @@
 -- Add any additional keymaps here
 -- Use jk to escape insert mode
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+
+-- Tmux copy/paste integration
+-- Copy visual selection to tmux buffer and send to system clipboard via OSC 52
+vim.keymap.set("v", "<leader>tc", function()
+  -- Yank to register 0
+  vim.cmd('normal! y')
+  local content = vim.fn.getreg('0')
+  -- Load into tmux buffer
+  vim.fn.system("tmux load-buffer -", content)
+  -- Send OSC 52 sequence to sync with system clipboard
+  local osc52 = require("vim.ui.clipboard.osc52")
+  local lines = vim.split(content, "\n")
+  osc52.copy("+")(lines)
+  -- Restore visual selection
+  vim.cmd('normal! gv')
+end, { noremap = true })
+-- Paste from tmux buffer
+vim.keymap.set("n", "<leader>tp", ':let @0 = system("tmux save-buffer -")<cr>"0p<cr>g;', { noremap = true })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,21 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- Use system clipboard
-vim.opt.clipboard = "unnamedplus"
-
--- Prefer OSC52 so copy works from remote/docker terminals
-vim.g.clipboard = {
-  name = "OSC52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
+-- Don't use system clipboard by default (use <leader>tc for explicit sync)
+-- Override LazyVim's default which auto-enables clipboard sync
+vim.opt.clipboard = "" -- Keep yanks in vim registers only
 
 -- Auto-reload files when changed on disk
 vim.opt.autoread = true
